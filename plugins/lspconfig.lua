db4a5d3ee0819418dac28a40f07a74814fde372c
Aravindhan Elayakumar <35667987+rhisling@users.noreply.github.com>
2023-01-17 13:29:46 -0800

    Update lspconfig.lua

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -12,3 +12,27 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+
+-- rust lsp config
+lspconfig.rust_analyzer.setup({
+  on_attach=on_attach,
+  settings = {
+      ["rust-analyzer"] = {
+          imports = {
+              granularity = {
+                  group = "module",
+              },
+              prefix = "self",
+          },
+          cargo = {
+              buildScripts = {
+                  enable = true,
+              },
+          },
+          procMacro = {
+              enable = true
+          },
+      }
+  }
+})
